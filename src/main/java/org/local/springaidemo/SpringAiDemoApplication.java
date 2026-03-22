4a5420d7c5c94b3d1891dc9c507cc8e0545e3420
Shalini Mishra <Shalinimishra231@gmail.com>
2026-03-22 01:21:17 -0400

    test commit

--- src/main/java/org/local/springaidemo/SpringAiDemoApplication.java
+++ src/main/java/org/local/springaidemo/SpringAiDemoApplication.java
@@ -10,9 +10,6 @@ public class SpringAiDemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringAiDemoApplication.class, args);
-        System.out.println();
-        List<Integer> numbers = List.of(1, 2, 3, 4, 5);
-
     }
 
 }
